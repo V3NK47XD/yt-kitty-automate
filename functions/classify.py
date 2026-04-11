f1a1b867af997dfedfc2f10574b0152c4fd9533b
V3NK47-ASUS <venkatprashadalwar@gmail.com>
2026-04-11 22:36:49 +0530

    video caption done

--- functions/classify.py
+++ functions/classify.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import ffmpeg
 
 import json
+from google import genai
 
 load_dotenv()
 
@@ -48,6 +49,24 @@ def get_all_videos(base_folder="buffer"):
 
     return all_videos
 
+def remove_audio(video_path):
+    dir_name = os.path.dirname(video_path)
+    base_name = os.path.basename(video_path)
+    output_path = os.path.join(dir_name, f"temp_muted_{base_name}") if dir_name else f"temp_muted_{base_name}"
+    
+    try:
+        (
+            ffmpeg
+            .input(video_path)
+            .output(output_path, vcodec='copy', an=None)
+            .run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
+        )
+    except ffmpeg.Error as e:
+        print("FFmpeg Error:", e.stderr.decode() if e.stderr else e)
+        raise
+        
+    return output_path
+
 def classify(video):
 
     categories = os.getenv("CATEGORIES").split(",")
@@ -55,17 +74,15 @@ def classify(video):
 
     print("Available categories:", categories)
 
-    img = getimage(video)
-    img_b64 = base64.b64encode(img).decode()
 
     prompt = f"""
-You are a classifier. AND FOLLOW STRICT INSTUCTION. OUPUT ONLY JSON AND NOTHING ELSE.
+You are a classifier and video describer. AND FOLLOW STRICT INSTUCTION. OUPUT ONLY JSON AND NOTHING ELSE.
+Make a high descriptive caption for the given video. And add a bit of humor, or make the caption catchy. Strict 2 word caption limit.
 
 Choose ONE category from this list:
 {categories}
 And don't use the below for caption:
 {get_all_videos()}
-Litteraly dont even include the thing process or anything.
 ONLY OUTPUT THE JSON, NOT ANYTHING ELSE. JUST THE JSON FORMAT SPECIFIED BELOW.
 Return ONLY strict JSON in this format:
 {{
@@ -74,40 +91,26 @@ Return ONLY strict JSON in this format:
 }}
 """
     print("sending to gemma")
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemma-4-31b-it:generateContent?key={API_KEY}"
-
-    payload = {
-        "contents": [
-            {
-                "parts": [
-                    {"text": prompt,"thought":"false"},
-                    {
-                        "inline_data": {
-                            "mime_type": "image/jpeg",
-                            "data": img_b64
-                        }
-                    }
-                ]
-            }
-        ],
-        "generationConfig": {
-            "thinkingConfig": {
-                "thinkingLevel": "MINIMAL"
-      }
-    }
-    }
-    print(payload)
-    r = requests.post(url, json=payload)
-    data = r.json()
-    print(data)
-    parts = data["candidates"][0]["content"]["parts"]
-
-    # find the part that actually has text
-    text = None
-    for p in parts:
-        if "text" in p and p["text"].strip():
-            text = p["text"]
-            break
+    client = genai.Client(api_key=API_KEY)
+    
+    video_file_name = remove_audio(video)
+    video_bytes = open(video_file_name, 'rb').read()
+    
+    if os.path.exists(video_file_name):
+        os.remove(video_file_name)
+    response = client.models.generate_content(
+        model='gemma-4-31b-it',
+        contents=genai.types.Content(
+            parts=[
+                genai.types.Part(
+                    inline_data=genai.types.Blob(data=video_bytes, mime_type='video/mp4')
+                ),
+                genai.types.Part(text=prompt)
+            ]
+        )
+    )
+    
+    text = response.text
 
     # clean markdown ```json ```
     text = text.strip().replace("```json", "").replace("```", "").strip()
